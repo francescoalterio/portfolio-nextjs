bc4e77e405c731d2d7bbb6e6565b3208f5be6cf1
Francesco Alterio <francescoalteriog@gmail.com>
2022-07-01 15:21:49 -0400

    add politicas manejador de proyectos

---
+++ pages/politicasprojectmanager.js
@@ -0,0 +1,32 @@
+export default function politicasprojectmanager() {
+  return (
+    <div style={{ marginLeft: 20, marginTop: 20 }}>
+      <h1 style={{ marginBottom: 10 }}>Politicas de Privacidad</h1>
+      <p style={{ marginBottom: 10 }}>
+        Manejador de Proyectos no recoge ningún dato personal de los usuarios.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        Manejador de Proyectos no registra direcciones IP.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        Manejador de Proyectos no accede a los datos de contactos del
+        dispositivo.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        Manejador de Proyectos no accede a las cuentas de correos de los
+        usuarios.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        Manejador de Proyectos no guarda datos relativos al dispositivo.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        Fran Studios, no Google, es el único responsable del contenido de Fran
+        Studios.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        Para más información sobre la presente política de privacidad puede
+        dirigirse a francescoaltx5@gmail.com
+      </p>
+    </div>
+  );
+}
