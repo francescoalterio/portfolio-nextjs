ca052be1e756f1400ade55431841e99d1d6341b6
Francesco Alterio <francescoalteriog@gmail.com>
2022-07-13 14:38:41 -0400

    d

--- components/Youtube.js
+++ components/Youtube.js
@@ -11,7 +11,7 @@ const Youtube = ({ videos }) => {
         </div>
         <hr className="separator" />
         <div className="box-videos">
-          {videos.map((video) => (
+          {videos?.map((video) => (
             <Video youtubeVideo={video} />
           ))}
         </div>
