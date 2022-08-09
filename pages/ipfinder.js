7e847a97481901de2d0afac57045a2a8bc2a8154
Francesco Alterio <francescoalteriog@gmail.com>
2022-08-09 15:53:09 -0400

    add ip finder policies

---
+++ pages/ipfinder.js
@@ -0,0 +1,28 @@
+export default function ipfinder() {
+  return (
+    <div style={{ marginLeft: 20, marginTop: 20 }}>
+      <h1 style={{ marginBottom: 10 }}>Privacy policies</h1>
+      <p style={{ marginBottom: 10 }}>
+        IP Finder does not collect any personal data from users.
+      </p>
+      <p style={{ marginBottom: 10 }}>IP Finder does not log IP addresses.</p>
+      <p style={{ marginBottom: 10 }}>
+        IP Finder does not access the contact data of the device.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        IP Finder does not access users email accounts.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        IP Finder does not save data related to the device.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        Fran Studios, not Google, is solely responsible for the content of Fran
+        Studios.
+      </p>
+      <p style={{ marginBottom: 10 }}>
+        For more information about this privacy policy, you can contact
+        francescoaltx5@gmail.com
+      </p>
+    </div>
+  );
+}
